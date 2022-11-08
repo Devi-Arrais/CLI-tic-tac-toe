c453aeb8ba0288b06caef74fd290980996030ddb
devi44 <deviarrais34@gmail.com>
2022-11-08 19:19:36 -0300

    Indentização

--- src/game.rs
+++ src/game.rs
@@ -1,14 +1,15 @@
 use std::fmt::*;
 
-
-
-pub enum Status{
-    X, O, Nulo
+#[derive(PartialEq)]
+pub enum Status {
+    X,
+    O,
+    Nulo,
 }
 
-impl Status{
-    fn to_string(&self)-> String{
-        match self{
+impl Status {
+    fn to_string(&self) -> String {
+        match self {
             Status::X => " X ".to_string(),
             Status::O => " O ".to_string(),
             Status::Nulo => "   ".to_string(),
@@ -16,60 +17,74 @@ impl Status{
     }
 }
 
-impl Debug for Status{
+impl Debug for Status {
     fn fmt(&self, f: &mut Formatter) -> Result {
         write!(f, "{}", self.to_string())
     }
 }
 
-pub struct Velha{
+pub struct Velha {
     campo: [Status; 9],
 }
-impl Velha{
-    pub fn new()-> Velha{
-        Velha { campo: [Status::Nulo,Status::Nulo,Status::Nulo,
-                        Status::Nulo,Status::Nulo,Status::Nulo,
-                        Status::Nulo,Status::Nulo,Status::Nulo] }
+impl Velha {
+    pub fn new() -> Velha {
+        Velha {
+            campo: [
+                Status::Nulo,
+                Status::Nulo,
+                Status::Nulo,
+                Status::Nulo,
+                Status::Nulo,
+                Status::Nulo,
+                Status::Nulo,
+                Status::Nulo,
+                Status::Nulo,
+            ],
+        }
     }
-    pub fn campo(&self)-> String{
-       let mut map = "   |   |   \n".to_string();
-       let mut i = 0;
-       while i < 8{
-           let fmt = format!("{:?}|{:?}|{:?}\n", self.campo[i], self.campo[i + 1], self.campo[i + 2]);
-           map.push_str(&fmt);
+    pub fn campo(&self) -> String {
+        let mut map = "   |   |   \n".to_string();
+        let mut i = 0;
+        while i < 8 {
+            let fmt = format!(
+                "{:?}|{:?}|{:?}\n",
+                self.campo[i],
+                self.campo[i + 1],
+                self.campo[i + 2]
+            );
+            map.push_str(&fmt);
 
-           if i < 6{
-               map.push_str("---|---|---\n");
-           }else {
-               map.push_str("   |   |   \n");
-           }
-           i += 3;
-       }
-     map
+            if i < 6 {
+                map.push_str("---|---|---\n");
+            } else {
+                map.push_str("   |   |   \n");
+            }
+            i += 3;
+        }
+        map
     }
-    pub fn player1(&mut self, posicao: u8, play: Status){
+    pub fn player1(&mut self, posicao: u8, play: Status) {
         let jogada: u8 = posicao - 1;
-        loop{
-            if self.campo[jogada as usize] == Status::Nulo{
-               self.campo[jogada as usize] = play;
-               break;
-
-            }else{
+        loop {
+            if self.campo[jogada as usize] == Status::Nulo {
+                self.campo[jogada as usize] = play;
+                break;
+            } else {
                 println!("Posição invalida escolha novamente");
                 continue;
             }
         }
     }
-    pub fn player2(&mut self, pos: u8, player: Status){
+    pub fn player2(&mut self, pos: u8, player: Status) {
         let zone: u8 = pos - 1;
-        loop{
-            if self.campo[zone as usize] == Status::Nulo{
-                 self.campo[zone as usize] = player;
-                 break;
-            }else{
+        loop {
+            if self.campo[zone as usize] == Status::Nulo {
+                self.campo[zone as usize] = player;
+                break;
+            } else {
                 println!("Posição ocupada escolha novamente");
                 continue;
             };
         }
-   }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,37 @@ use game::Velha;
 use std::io;
 
 fn main() {
-   let mut game = Velha::new();
-   //std::process::Command::new("clear").status().unwrap(); 
-   println!("{}", game.campo());
-   let mut jogador = 1; 
-   loop{
-      if jogador == 1{
-      println!("Digite a posição que deseja: ");
+    let mut game = Velha::new();
+    //std::process::Command::new("clear").status().unwrap();
+    println!("{}", game.campo());
+    let mut jogador = 1;
+    loop {
+        if jogador == 1 {
+            println!("Digite a posição que deseja: ");
 
-      let mut posicao = String::new();
-      io::stdin().read_line(&mut posicao).ok().expect("Não foi possivel pega a posição");
-      let conv: u8 = posicao.trim().parse().unwrap();
-      game.player1(conv, game::Status::X);
-     // std::process::Command::new("clear").status().unwrap();
-      println!("{}", game.campo());
-      jogador += 1;
-      }else{
-      println!("Digite a posição que deseja: ");
+            let mut posicao = String::new();
+            io::stdin()
+                .read_line(&mut posicao)
+                .ok()
+                .expect("Não foi possivel pega a posição");
+            let conv: u8 = posicao.trim().parse().unwrap();
+            game.player1(conv, game::Status::X);
+            // std::process::Command::new("clear").status().unwrap();
+            println!("{}", game.campo());
+            jogador += 1;
+        } else {
+            println!("Digite a posição que deseja: ");
 
-      let mut posicao = String::new();
-      io::stdin().read_line(&mut posicao).ok().expect("Não foi possivel pega a posição");
-      let conv: u8 = posicao.trim().parse().unwrap();
-      game.player1(conv, game::Status::O);
-     // std::process::Command::new("clear").status().unwrap();
-      println!("{}", game.campo());
-      jogador -= 1;
-
-      }
-}
+            let mut posicao = String::new();
+            io::stdin()
+                .read_line(&mut posicao)
+                .ok()
+                .expect("Não foi possivel pega a posição");
+            let conv: u8 = posicao.trim().parse().unwrap();
+            game.player2(conv, game::Status::O);
+            // std::process::Command::new("clear").status().unwrap();
+            println!("{}", game.campo());
+            jogador -= 1;
+        }
+    }
 }
